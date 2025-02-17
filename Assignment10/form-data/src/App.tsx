8076b4f082b5c64603bce0938e80752f9c8838c3
Sachinborade07 <sachinborade195@gmail.com>
2025-02-17 23:28:49 +0530

    Assignment10

---
+++ Assignment10/form-data/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Form from './form';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1> INFORMATION FORM </h1>
+      <Form />
+    </div >
+  );
+};
+
+export default App;
---
+++ Assignment10/form-data/src/form.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+const Form: React.FC = () => {
+    const [formData, setFormData] = useState({
+        first_name: "",
+        last_name: "",
+        age: "",
+        gender: "",
+        skills: "",
+        email: "",
+        phone: "",
+        address: "",
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleEvent = (e: React.FormEvent) => {
+        e.preventDefault();
+        alert(JSON.stringify(formData));
+    };
+
+    return (
+        <form onSubmit={handleEvent}>
+            <input type="text" name="first_name" placeholder="First Name" value={formData.first_name} onChange={handleChange} required />
+            <input type="text" name="last_name" placeholder="Last Name" value={formData.last_name} onChange={handleChange} required />
+            <input type="number" name="age" placeholder="Age" value={formData.age} onChange={handleChange} required />
+
+            <label>
+                <input type="radio" name="gender" value="Male" checked={formData.gender === "Male"} onChange={handleChange} /> Male
+            </label>
+            <label>
+                <input type="radio" name="gender" value="Female" checked={formData.gender === "Female"} onChange={handleChange} /> Female
+            </label>
+
+            <select name="skills" value={formData.skills} onChange={handleChange}>
+                <option value="">Select Skill</option>
+                <option value="React">React</option>
+                <option value="JS">JS</option>
+                <option value="PYTHON">PYTHON</option>
+            </select>
+
+            <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} required />
+            <input type="number" name="phone" placeholder="Phone Number" value={formData.phone} onChange={handleChange} required />
+
+            <button type="submit">Submit</button>
+        </form>
+    )
+
+};
+
+export default Form;
